81f4bfa628b219596339d7bdce0cf3cd980753c8
nkscoder <nkscoder@gmail.com>
2016-08-24 11:33:49 +0530

    update facebook and google urls

--- modules/home/controllers/home.php
+++ modules/home/controllers/home.php
@@ -681,7 +681,7 @@ include_once("./facebook/inc/facebook.php"); //include facebook SDK
 ######### Facebook API Configuration ##########
 $appId = '266150530435918'; //Facebook App ID
 $appSecret = '8a40cba44c6c71e03332174a7e60f80c'; // Facebook App Secret
-$homeurl = 'http://gobarra.com/gobarra/facebook/';  //return to home
+$homeurl = 'http://gobarra.com/facebook/';  //return to home
 $fbPermissions = 'email';  //Reomequired facebook permissions
 
 //Call Facebook API
