b4cefc61fbe9573fa03af91fed3d663a410a4319
nkscoder <nkscoder@gmail.com>
2016-09-13 13:05:16 +0530

    messages page update code

--- modules/messages/controllers/messages.php
+++ modules/messages/controllers/messages.php
@@ -404,37 +404,7 @@ class Messages extends Public_Controller
 											<span><p><a><img class="group2" width="40%" href="<?php echo $Image; ?>" src="<?php echo $Image; ?>">/a></p></span>
 										</div>
 										<?php
-										} ?>
-																															   <!-- Modal -->
-
-                                             <!--.................................................................-->
-
-										<!--
-										<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-											Launch demo modal
-										</button>
-
-
-										<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" onclick="modaldiv()">
-											<div class="modal-dialog" role="document">
-												<div class="modal-content">
-													<div class="modal-header">
-														<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-														<h4 class="modal-title" id="myModalLabel">Modal title</h4>
-													</div>
-													<div class="modal-body">
-														...sddddddddddddddddddddd
-													</div>
-													<div class="modal-footer">
-														<button type="button" class="btn btn-default" data-dismiss="modal" >Close</button>
-														<button type="button" class="btn btn-primary">Save changes</button>
-													</div>
-												</div>
-											</div>
-										</div>
--->
-
-										<!--..............................................................................-->
+										} ?>														
 
 
 									</div>
